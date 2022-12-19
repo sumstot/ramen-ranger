d663b80b68dcc65567a98254d62d42c1f41109c5
Soren Umstot <soren.umstot@gmail.com>
2022-12-19 19:17:16 +0900

    Added to the bowls controller

--- app/controllers/bowls_controller.rb
+++ app/controllers/bowls_controller.rb
@@ -1,2 +1,30 @@
 class BowlsController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = current.user
+    @bowl = Bowl.new
+  end
+
+  def create
+    @user = current.user
+    @bowl = Bowl.new(bowl_params)
+    @bowl.restaurant = @restaurant
+    if @bowl.save
+      redirect_to #somewhere rails paths
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @bowl = Bowl.find(params[:id])
+  end
+
+  private
+  
+  def bowl_params
+    params.require(:bowl).permit(:soup, :score, :description, :review)
+  end
 end
