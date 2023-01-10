3fd0cb40cf70e47bec44ac9f1aea6634ac2465fd
Soren Umstot <soren.umstot@gmail.com>
2023-01-10 18:30:57 +0900

    Maps and geocoding are WORKING! Stimulus required some weird syntax for values and #private methods did not work

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,9 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = {
-    apiKey: String,
-    markers: Array
+
+  static get values() {
+    return {
+      apiKey: String,
+      markers: Array
+    }
   }
   connect() {
     console.log(`Map controller connected`)
@@ -14,22 +17,22 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({ container: this.element, style: "mapbox://styles/mapbox/streets-v10"
     })
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-    
+    this.addMarkersToMap()
+    this.fitMapToMarkers()
+
+  }
+  /////////////// Private ///////////////
+  addMarkersToMap() {
+  this.markersValue.forEach((marker) =>{
+    new mapboxgl.Marker()
+    .setLngLat([marker.lng, marker.lat])
+    .addTo(this.map)
+  })
   }
 
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) =>{
-      new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
-      .addTo(this.map)
-    })
-  }
-
-  #fitMapToMarkers(){
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  fitMapToMarkers(){
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
