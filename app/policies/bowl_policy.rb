661e9f161019daeddae74a089c6667dbe840120f
Soren Umstot <soren.umstot@gmail.com>
2023-01-25 15:26:16 +0900

    added pundit policy files for bowl model and restaurant model.

---
+++ app/policies/bowl_policy.rb
@@ -0,0 +1,29 @@
+class BowlPolicy < ApplicationPolicy
+  attr_reader :user
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def initialize(user)
+    @user = user
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+end
