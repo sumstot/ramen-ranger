5891d75f67821f460553e985bdc3869b0d6fdd78
Soren Umstot <soren.umstot@gmail.com>
2023-01-25 11:16:45 +0900

    Generated a rails migration to add pg_search multisearch to the project

--- app/models/bowl.rb
+++ app/models/bowl.rb
@@ -1,4 +1,10 @@
 class Bowl < ApplicationRecord
   belongs_to :restaurant
   has_one_attached :image
+  include PgSearch::Model
+  pg_search_scope :search_by_soup_and_restauarant,
+  against: [:soup, :restaurant]
+  using: {
+    tsearch: { prefix: true }
+  }
 end
