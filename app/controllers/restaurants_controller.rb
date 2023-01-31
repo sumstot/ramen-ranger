17c46cb329a07093e62af3f76c80975ee621f8fd
Soren Umstot <soren.umstot@gmail.com>
2023-01-31 09:45:58 +0900

    added restaurant params

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,10 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :jpn_name, :address, :average_score, :year_opened, :city, :prefecture, :station, :latitude, :longitude)
+  end
 end
